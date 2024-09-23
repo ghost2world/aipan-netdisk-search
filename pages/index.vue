4e81edf39baf577920b18a3e371e548c16c6fd9b
ghost2world <btmcheck8@gmail.com>
2024-09-23 20:54:59 +0800

    百度tongji

--- pages/index.vue
+++ pages/index.vue
@@ -45,6 +45,16 @@ onMounted(async () => {
   }
 })
 </script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?97171b6e4c6a0fcb2b1e215c700886fe";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
 
 <template>
   <div class="bg-[#ffffff] dark:bg-gray-800  min-h-screen py-[60px]">
